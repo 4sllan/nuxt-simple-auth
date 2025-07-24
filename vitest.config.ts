c10181a19d5f989bcf7f8c57996ead5eac0c3c4b
aslan.gama <aslan.gama@sixchains.com.br>
2025-07-24 18:34:32 -0300

    v1.7.2

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '#imports': fileURLToPath(new URL('./.nuxt/imports.d.ts', import.meta.url)),
+      '#app': fileURLToPath(new URL('./.nuxt/app.d.ts', import.meta.url)),
       '#auth-utils': fileURLToPath(new URL('./src/runtime/utils/index.ts', import.meta.url)),
     },
   },
